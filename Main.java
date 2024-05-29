35388547f733856983956a8867212261dddabba3
meghanasiddha17 <meghanasiddha17@gmail.com>
2024-05-29 11:54:16 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int passwordLength = 12;
+            String password = PasswordGenerator.generatePassword(passwordLength);
+            System.out.println("Generated Password: " + password);
+
+            SecretKey secretKey = PasswordEncryptor.generateSecretKey();
+            String encryptedPassword = PasswordEncryptor.encryptPassword(password, secretKey);
+            System.out.println("Encrypted Password: " + encryptedPassword);
+
+            // Optionally, decrypt the password to verify
+            String decryptedPassword = PasswordEncryptor.decryptPassword(encryptedPassword, secretKey);
+            System.out.println("Decrypted Password: " + decryptedPassword);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
